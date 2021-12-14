4d32f026d1ad631da87283f0aa4a4578d6a8e38c
iormat <iorio436@outlook.it>
2021-12-14 20:17:28 +0100

    Major: AppMainGallerySection update

--- src/components/AppMainGallerySection.vue
+++ src/components/AppMainGallerySection.vue
@@ -19,6 +19,24 @@
 				</button>
 			</div>
         </div>
+        <!-- jumbotron -->
+        <div class="jumbotron">
+            <div class="container_small">
+                <div>
+                    <img src="../assets/img/quote_alt.png" alt="quotes">
+                </div>
+                <h2>I have to say that I have 2 children ages 5 and 2 and have used various daycare's in Kindergartens and this is by far the very best I have ever used.</h2>
+                <span class="underline"></span>
+                <span class="sign">Cecil J&period; Kirk</span>
+                <!-- empty divs to make rectangles -->
+                <div class="rectangle"></div>
+                <div class="rectangle"></div>
+                <div class="rectangle"></div>
+                <div class="rectangle"></div>
+                <div class="rectangle"></div>
+                <div class="rectangle"></div>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -63,8 +81,8 @@ export default {
                 padding: 2em;
                 border-radius: 50%;
                 img {
-                    width: 3rem;
-                    height: 3rem;
+                    width: 2.5rem;
+                    height: 2.5rem;
                 }
             }           
             &:hover .add_photo {
@@ -90,6 +108,51 @@ export default {
             font-weight: 400;
         }
     }
-
-
+    .jumbotron {
+        padding: 2rem;
+        background-image: url('../assets/img/parallax_02.jpg');
+        background-size: cover;
+        background-position: center;
+        .container_small {
+            text-align: center;
+            & div:first-child {
+                display: inline-block;
+                border-radius: 50%;
+                border: 2px solid var(--clr-primary-100);
+                padding: 2rem;
+            }
+            img {
+                display: block;
+                width: 3rem;
+                height: 3rem;
+            }
+            h2 {
+                color: var(--clr-primary-100);
+                font-size: 2.18rem;
+                font-weight: 300;
+                margin: 2rem 0 0 0;
+            }
+            .underline {
+                display: inline-block;
+                margin: 2.5rem;
+                border: 1px solid var(--clr-primary-100);
+                width: 10ch;
+            }
+            .sign {
+                display: block;
+                color: var(--clr-primary-100);
+            }
+            .rectangle {
+                display: inline-block;
+                height: .75rem;
+                width: 2.3rem;
+                border: 1px solid var(--clr-primary-100);
+                margin: 2rem .25rem;
+                &:nth-of-type(4) {
+                    position: relative;
+                    bottom: .7rem;
+                }
+            }
+        }
+    }
 </style>
