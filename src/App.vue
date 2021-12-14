2139b6868452699efa6625ef6e9987eddb192e2d
iormat <iorio436@outlook.it>
2021-12-14 10:43:35 +0100

    Minor: add AppMain component

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,20 @@
 		<AppHeader
 		:linksArr="navLinksArr"
 		/>
+
+		<AppMain/>
 	</div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
 
 export default {
 	name: 'App',
 	components: {
 		AppHeader,
+		AppMain,
 	},
 	data() {
 		return {
---
+++ src/components/AppMain.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        
+    </main>
+</template>
+
+
+<script>
+export default {
+    name: 'AppMain',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
